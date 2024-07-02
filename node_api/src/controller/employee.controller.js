a6ea363f47244ad8442054c3caa4b51247c0e98d
chanthorn-web <kimchanthorn69@gmail.com>
2024-07-02 17:07:47 +0700

    api update data in DB

--- node_api/src/controller/employee.controller.js
+++ node_api/src/controller/employee.controller.js
@@ -36,9 +36,6 @@ const getOne = (req,res)=>{
 }
 
 const create = (req,res)=>{
-
-
-  
     const {
             firstname,
             lastname,
@@ -67,9 +64,42 @@ const create = (req,res)=>{
 }
 
 
+const update = (req,res)=>{
+    const {
+        employee_id,
+        firstname,
+        lastname,
+        tel,
+        email,
+        base_salary,
+        address,
+        province,
+        country
+    } = req.body
+
+    var sql = "UPDATE employee SET firstname=?, lastname=?, tel=?,  email=?, base_salary=? ,address=?, province=?, country=? WHERE employee_id=?";
+    var param_sql = [firstname,lastname,tel,email,base_salary,address,province,country,employee_id]
+
+    db.query(sql,param_sql,(error,row)=>{
+        if(error){
+            res.json({
+                message:error,
+                error: true
+            })
+        }
+        else{
+            res.json({
+                message: "Update successfuly !!",
+                data: row
+            })
+        }
+    })
+}
+
 
 module.exports = {
     getAll,
     getOne,
-    create
+    create,
+    update
 }
--- node_api/src/route/employee.route.js
+++ node_api/src/route/employee.route.js
@@ -5,6 +5,7 @@ const employee = (app) => {
     app.get("/api/employee", ct.getAll)
     app.get("/api/employee/:id", ct.getOne)
     app.post("/api/employee",ct.create)
+    app.put("/api/employee",ct.update)
 }
 
 module.exports = employee;
