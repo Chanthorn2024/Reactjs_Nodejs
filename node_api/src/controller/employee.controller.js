6fe0808f56ecd5e84644eafb7c0f36ec55dab681
chanthorn-web <kimchanthorn69@gmail.com>
2024-07-03 11:18:05 +0700

    api removed data from from DB (from pc-2 branch)

--- node_api/src/controller/employee.controller.js
+++ node_api/src/controller/employee.controller.js
@@ -35,6 +35,7 @@ const getOne = (req,res)=>{
      })
 }
 
+
 const create = (req,res)=>{
     const {
             firstname,
@@ -46,6 +47,26 @@ const create = (req,res)=>{
             province,
             country
           } = req.body
+     // end check ehich field Required 
+     var message = {} 
+        if(firstname == null || firstname == ""){
+            message.firstname = "firstname required!"
+        }
+        if(lastname == null || lastname == ""){
+            message.lasstname = "lastname required!"
+        }
+        if(tel == null || tel == ""){
+             message.tel = "telephone required!"
+        }
+       //Object.keys(message).length = 3  
+       //Object.keys(message).length => return lenght of  object message 
+      if(Object.keys(message).length > 0){
+        res.json({
+            error: true,
+            Message : message
+        })
+        return //
+      }  
 
      var sql = "INSERT INTO employee (firstname, lastname, tel, email, base_salary, address, province, country) VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
      var param_data = [firstname,lastname,tel,email,base_salary,address,province,country]
@@ -89,7 +110,7 @@ const update = (req,res)=>{
         }
         else{
             res.json({
-                message: "Update successfuly !!",
+                message: row.affectedRows ? "Update successfuly !!": "Data not in System!",
                 data: row
             })
         }
@@ -99,18 +120,21 @@ const update = (req,res)=>{
 const remove =(req,res)=>{
     //var id = req.params.id
     var {id} = req.params
+    // var {id} = req.query
     var sql = "DELETE FROM employee WHERE employee_id= ?"
     //var param_sql = [id]
     db.query(sql,[id],(error,row)=>{
-        if(error){
+        if(!error){
+            res.json({
+                // message: (row.affectedRows!= 0) ? "Deleted Successfull!" : "Data not in system",
+                message: (row.affectedRows) ? "Deleted Successfull!" : "Data not in system",
+                data : row
+            })
+            
+        }else{
             res.json({
                 message: error,
                 error: true
-            })
-        }else{
-            res.json({
-                message: "Deleted Successful...!!!",
-                data : row
             })
         }
     })
