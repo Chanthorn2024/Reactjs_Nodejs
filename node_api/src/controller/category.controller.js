8c67d290c0f53231dc5ae9d9f1f3f312fa566aef
Chanthorn2024 <kimchanthon46@gmail.com>
2024-07-04 11:59:25 +0700

    api Category CRUD

--- node_api/src/controller/category.controller.js
+++ node_api/src/controller/category.controller.js
@@ -21,22 +21,131 @@ const getAll = (req,res)=>{
 
 // fuction Get One Data from Table"
 const getOne=(req,res)=>{
-
+    var id = req.params.id
+     var sql = "SELECT * FROM category WHERE category_id=?"
+     db.query(sql,[id],(error,row)=>{
+        if(error){
+            res.json({
+                message:error,
+                error:true
+            })
+        }else{
+            res.json({
+                list:row
+            })
+        }
+     })
 }
 
 // fuction Get Create Data from Table"
 const create = (req,res)=>{
+    
+        const {
+                name,
+                description,
+                parent_id,
+                status
+              } = req.body
 
+         // end check ehich field Required 
+         var message = {} 
+            /* USE fuction by IsEmpty */
+            if(isEmptyOrNull(name)){
+                message.name = "Category name required!"
+                res.json({
+                    error: true,
+                    Message : message
+                })
+                return 
+            }
+     
+    
+         var sql = "INSERT INTO category (name, description, parent_id, status) VALUES(?, ?, ?, ?)"
+         var param_data = [name, description, parent_id, status]
+         db.query(sql,param_data,(error,row)=>{
+            if(error){
+                res.json({
+                    message : error,
+                    error : true
+                })
+            }else{
+                res.json({
+                    message:"Category Created Successfuly!!"
+                })
+            }
+         })    
 }
 
 // fuction Get Update Data from Table"
 const update = (req,res)=>{
+    const {
+             category_id,
+             name,
+             description,
+             parent_id,
+             status
+    } = req.body
 
+    var message = {} 
+    /* USE fuction by IsEmpty */
+    if(isEmptyOrNull(name)){
+        message.name = "Category name required!"
+        res.json({
+            error: true,
+            Message : message
+        })
+        return 
+    }
+    else if(isEmptyOrNull(category_id)){
+        message.category_id = "Category id required!"
+        res.json({
+            error: true,
+            Message : message
+        })
+        return 
+    }
+
+    var sql = "UPDATE category SET category_id=?,name=?,description=?,parent_id=?,status=? WHERE category_id=?";
+    var param_sql = [category_id,name,description,parent_id,status, category_id]
+
+    db.query(sql,param_sql,(error,row)=>{
+        if(error){
+            res.json({
+                message:error,
+                error: true
+            })
+        }
+        else{
+            res.json({
+                message: row.affectedRows ? "Update successfuly !!": "Data not in System!",
+                data: row
+            })
+        }
+    })
 }
 
 // fuction Get Delete Data from Table"
-const remove = (req,res) => {
-
+const remove = (req,res) => { 
+     //var id = req.params.id
+     var {id} = req.params
+     // var {id} = req.query
+     var sql = "DELETE FROM category WHERE category_id= ?"
+     //var param_sql = [id]
+     db.query(sql,[id],(error,row)=>{
+         if(!error){
+             res.json({
+                 // message: (row.affectedRows!= 0) ? "Deleted Successfull!" : "Data not in system",
+                 message: (row.affectedRows) ? "Deleted Successfull!" : "Data not in system",
+                 data : row
+             })
+             
+         }else{
+             res.json({
+                 message: error,
+                 error: true
+             })
+         }
+     })
 }
 
 module.exports = {
